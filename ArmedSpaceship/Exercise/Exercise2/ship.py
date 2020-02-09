6d63d0764a2ef7dece42e27f4b7bfd5a792094bf
ywyz <admin@ywyz.tech>
2020-02-09 15:23:04 +0800

    Fix Exercise2

--- ArmedSpaceship/Exercise/Exercise2/ship.py
+++ ArmedSpaceship/Exercise/Exercise2/ship.py
@@ -14,25 +14,7 @@ class Ship():
         self.rect.center = self.screen_rect.center
         self.rect.bottom = 400
 
-        self.move_right = False
-        self.move_left = False
-        self.move_up = False
-        self.move_down = False
-        self.center = float(self.rect.center)
-        self.bottom = float(self.rect.bottom)
 
-    def update(self):
-        if self.move_left:
-            self.center -= 1
-        if self.move_right:
-            self.center += 1
-        if self.move_up:
-            self.bottom -= 1
-        if self.move_down:
-            self.bottom += 1
-
-        self.rect.center = self.center
-        self.rect.bottom = self.bottom
 
     def blitme(self):
         self.screen.blit(self.image, self.rect)
